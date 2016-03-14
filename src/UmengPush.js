1b5642ae737c0b7a1fc3d06f300e14d28cf3e435
JohnSmith <johnsmith1st@live.com>
2016-03-14 18:38:57 +0800

    [add] umeng push class definition

---
+++ src/UmengPush.js
@@ -0,0 +1,53 @@
+'use strict';
+
+let Promise = require('bluebird');
+
+class UmengPush {
+
+  constructor(args) {
+
+  }
+
+  pushAndroid(args, callback) {
+
+  }
+
+  pushAndroidAsync(args) {
+
+  }
+
+  pushIOS(args, callback) {
+
+  }
+
+  pushIOSAsync(args) {
+
+  }
+
+  getStatus(args, callback) {
+
+  }
+
+  getStatusAsync(args) {
+
+  }
+
+  cancel(args, callback) {
+
+  }
+
+  cancelAsync(args) {
+
+  }
+
+  uploadFile(args, callback) {
+
+  }
+
+  uploadFileAsync(args) {
+
+  }
+
+}
+
+module.exports = UmengPush;
